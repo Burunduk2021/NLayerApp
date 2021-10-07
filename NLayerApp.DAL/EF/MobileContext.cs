59fdbfc1d503b46f91f854aff47d5d9b36f4ff37
Natalia Denisova <natalia.kotlenok@gmail.com>
2021-10-07 11:25:01 +0300

    Add project files.

---
+++ NLayerApp.DAL/EF/MobileContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+using NLayerApp.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NLayerApp.DAL.EF
+{
+    public class MobileContext : DbContext
+    {
+
+        public MobileContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=NATALIA-PC;Database=EfTestDb;Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Phone>().HasData(
+                new Phone[]
+                {
+                    new Phone { Id=1, Name = "Nokia Lumia 630", Company = "Nokia", Price = 220 },
+                    new Phone { Id=2, Name = "iPhone 6", Company = "Apple", Price = 320 },
+                    new Phone { Id=3, Name = "LG G4", Company = "lG", Price = 260 },
+                    new Phone { Id=4, Name = "Samsung Galaxy S 6", Company = "Samsung", Price = 300 }
+                }
+            );
+        }
+    }
+}
---
+++ NLayerApp.DAL/Migrations/20210924063417_DbInit.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NLayerApp.DAL.Migrations
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Phones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sum = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Phones_PhoneId",
+                        column: x => x.PhoneId,
+                        principalTable: "Phones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Phones",
+                columns: new[] { "Id", "Company", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Nokia", "Nokia Lumia 630", 220m },
+                    { 2, "Apple", "iPhone 6", 320m },
+                    { 3, "lG", "LG G4", 260m },
+                    { 4, "Samsung", "Samsung Galaxy S 6", 300m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PhoneId",
+                table: "Orders",
+                column: "PhoneId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Phones");
+        }
+    }
+}
